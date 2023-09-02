776837f2bf566b1d186e6dd86bbb3a9434055b9d
f0nc <f0nc@users.noreply.github.com>
2023-09-02 20:46:22 +0300

    Added todo list Entry model

---
+++ src/main/java/com/example/TodoList/entry/Entry.java
@@ -0,0 +1,32 @@
+package com.example.TodoList.entry;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Entry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String description;
+
+    protected Entry() {}
+
+    public Entry(String username, String description) {
+        this.username = username;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Long getId() { return id; }
+}
